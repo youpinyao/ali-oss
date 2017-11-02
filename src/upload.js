44151edb7e12ad36530aa09c6c6040513d3eb2be
游品尧 <497400448@qq.com>
2017-11-02 16:34:20 +0800

    0.0.1

--- src/upload.js
+++ src/upload.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 const files = require('./files');
-const config = require('../demo/config.json');
 
-module.exports = function () {
+module.exports = function (config) {
   const store = oss(Object.assign({
 
   }, config));

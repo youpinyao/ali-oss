e588c58519237e2e79e96d917bbceee095030d93
游品尧 <497400448@qq.com>
2017-11-03 18:04:19 +0800

    1

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ module.exports = function (config) {
 
       files.forEach(file => {
         co(function* () {
-          return yield store.put(`${config.prefix}${file.split(config.srcDir)[1]}`, fs.createReadStream(file));
+          return yield store.put(path.join(config.prefix, file.split(config.srcDir)[1]), fs.createReadStream(file));
         }).then(data => {
           console.log(chalk.green(file));
           console.log(chalk.green(data.url));

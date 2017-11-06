bc173b3a079ba9cd494ede5a9c9dd86d63b16606
游品尧 <497400448@qq.com>
2017-11-06 16:55:24 +0800

    1

--- src/help.js
+++ src/help.js
@@ -14,6 +14,7 @@ version                   版本
   "accessKeySecret": "",// 必填 当设置access后可不填
   "bucket": "", // 必填 当设置access后可不填
   "region": "", // 选填 当设置access后可不填
+
   "srcDir": "./public/assets", // 必填 要上传的文件所在路径
   "ignoreDir": true, // 是否忽略文件夹
   "ignoreSuffix": "html,css", // 忽略后缀逗号隔开，或者数组
